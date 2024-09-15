7cc91789c5c05ab34801c49a7164bce9c58748c9
Ritesh Bakare <riteshbakare420@gmail.com>
2024-09-15 22:41:50 +0530

    additional routes added

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
 import pandas as pd
 import pickle
+import json
+import os
+from datetime import datetime
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 CORS(app)
+
+client = MongoClient("mongodb+srv://admin:admin@cluster0.cvj0l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client["cluster0"]  
+collection = db["values"]  
 
 with open('model.pkl', 'rb') as model_file:
     model = pickle.load(model_file)
@@ -13,13 +22,33 @@ with open('model.pkl', 'rb') as model_file:
 def home():
     return jsonify("Hello World DiabTech Backend!!! ")
 
+# Test route to check the connection
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({"message": "Connection successful"}), 200
+
+@app.route('/read', methods=['GET'])
+def read_records():
+    records = list(collection.find())  
+    for record in records:
+        record["_id"] = str(record["_id"])
+    return jsonify(records), 200
+
 @app.route("/data",methods=['POST'])
 def demo():
     data = request.json
     print("data recived from ardino: "+data)
     return jsonify({"data":data}),400
 
-@app.route("/check-glucose",methods=['POST'])
+# @app.route('/send_json',methods=['GET'])
+# def send_json_file():
+#     json_file_path = './public/glucose_results.json'
+#     with open(json_file_path, 'r') as f:
+#         json_data = json.load(f)
+
+#     return jsonify(json_data)
+
+@app.route("/check-glucose", methods=['POST'])
 def predictGlucose():
     try:
         data = request.json
@@ -35,12 +64,34 @@ def predictGlucose():
             except ValueError:
                 return jsonify({"error": f"'{value}' is not a valid float"}), 400
 
-        result = model.predict([float_data])  
+        result = model.predict([float_data])
 
-        # print(float_data)
-        # print(result)
+        output = {
+            "input": float_data,
+            "result": result[0],
+            "timestamp": datetime.now().isoformat()
+        }
 
-        return jsonify({"glucose result: ":result[0]}), 200
+        file_path = './public/glucose_results.json'
+        with open(file_path, 'a') as f:
+            json.dump(output, f)
+            f.write("\n")
+
+        mongo_document = {
+            "input": float_data,
+            "predicted_result": result[0],
+            "timestamp": datetime.now().isoformat()
+        }
+        
+        unique_id = "66e6ec78caff0600c6e447e3"
+
+        collection.update_one(
+            {"_id": unique_id},
+            {"$set": mongo_document},  
+            upsert=True  
+        )
+        
+        return jsonify({"result": result[0]}), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
